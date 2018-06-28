e0c140e50cf625cb81ba197ac4c230736c9c71df
Ongraph <sandeepmaity09@gmail.com>
2018-06-28 15:24:25 +0530

    timestamp updated

--- api/controllers/ChatController.js
+++ api/controllers/ChatController.js
@@ -479,6 +479,7 @@ const ChatController = () => {
         }
         let insertedMessageId;
         let insertedMessageInfo;
+        console.log("getT", getUTCTime());
         try {
             let message = {
                 user_id: parseInt(messageInfo.user_id),
